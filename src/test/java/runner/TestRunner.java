4068d349603ac4ecd5e91948c90b66510ae61018
Santosh1295 <chougulesantosh95@gmail.com>
2023-10-12 23:10:14 +0530

    first commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features",
+				 glue={"stepdefinitions","hooks"},
+				 publish=true,
+				 plugin={
+						 "pretty","html:target/CucumberReports/CucumberReport.html",
+						 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+						 "timeline:test-output-thread/"})
+public class TestRunner {
+	
+	
+
+}
